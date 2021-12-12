12936c425127e78d56a45d82b805a2b0ded66933
olzhiksdu <91359971+olzhiksdu@users.noreply.github.com>
2021-12-12 23:53:04 +0600

    Add files via upload

---
+++ VueJS/app.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+    
+    data() {
+        return{
+            firstName:'olzhik',
+            lastName: 'web developer',
+            email: '200103097@stu.sdu.edu.kz',
+            picture: 'img/olzhik v ochkah2.jpg',
+        }
+    }
+})
+
+app.mount('#app')
